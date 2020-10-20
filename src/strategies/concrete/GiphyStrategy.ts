910cfb7d6feb30b3307c3b30fd38851378376cb8
James Young <hello@jamesyoung.ch>
2020-10-21 01:33:47 +0200

    Add Giphy and Tenor Strategies

---
+++ src/strategies/concrete/GiphyStrategy.ts
@@ -0,0 +1,21 @@
+import {Gif} from "../../Gif";
+import {GiphyClient} from "../../clients";
+import {GifStrategy} from "../GifStrategy";
+
+export class GiphyStrategy implements GifStrategy {
+
+    readonly name: string = 'giphy';
+
+    public constructor(
+        private readonly client: GiphyClient
+    ) {}
+
+    public async search(query: string, limit: number): Promise<Array<Gif>> {
+        return this.client.search(query, limit);
+    }
+
+    public async trending(limit: number): Promise<Array<Gif>> {
+        return this.client.trending(limit);
+    }
+
+}
---
+++ src/strategies/concrete/TenorStrategy.ts
@@ -0,0 +1,21 @@
+import {Gif} from "../../Gif";
+import {TenorClient} from "../../clients";
+import {GifStrategy} from "../GifStrategy";
+
+export class TenorStrategy implements GifStrategy {
+
+    readonly name: string = 'tenor';
+
+    public constructor(
+        private readonly client: TenorClient
+    ) {}
+
+    public async search(query: string, limit: number): Promise<Array<Gif>> {
+        return this.client.search(query, limit);
+    }
+
+    public async trending(limit: number): Promise<Array<Gif>> {
+        return this.client.trending(limit);
+    }
+
+}
