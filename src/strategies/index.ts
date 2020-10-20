48cb1fc8a9af2f07b9cc46de9e5f91603240f61e
James Young <hello@jamesyoung.ch>
2020-10-21 01:33:47 +0200

    Add Giphy and Tenor Strategies

---
+++ src/strategies/index.ts
@@ -0,0 +1,6 @@
+// Strategy Interface
+export { GifStrategy } from './GifStrategy';
+
+// Concrete Strategies
+export { GiphyStrategy } from './concrete/GiphyStrategy';
+export { TenorStrategy } from './concrete/TenorStrategy';
