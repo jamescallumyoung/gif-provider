87dddd479af42143276ca51d2a9177a447eaf898
James Young <hello@jamesyoung.ch>
2020-10-21 01:46:16 +0200

    Add new GifProvider implementation
    - the new GifProvider is a class (not an interface) that provides methods
    for calling Search and Trending on its selected Strategies
    - uses the Strategy pattern, with a strategy for each Gif API
    - additionally, a Builder is provided for easy instantiation

---
+++ src/GifProviderBuilder.ts
@@ -0,0 +1,37 @@
+import {GifStrategy} from "./strategies";
+import {GifProvider} from "./GifProvider";
+
+export class GifProviderBuilder {
+
+    private strategies: Array<GifStrategy> = [];
+
+    /**
+     * Add a Strategy to the builder.
+     * @param strategy to add.
+     */
+    public addStrategy(strategy: GifStrategy): GifProviderBuilder {
+        this.strategies.push(strategy);
+        return this;
+    }
+
+    /**
+     * Remove a Strategy from the builder.
+     * N.B. This will remove _all_ strategies with the matching name
+     * @param name of the strategy to remove.
+     */
+    public removeStrategy(name: string): GifProviderBuilder {
+        this.strategies = this.strategies.filter(strategy => strategy.name !== name);
+        return this;
+    }
+
+    public listStrategies(): Array<string> {
+        return this.strategies.map(strategy => strategy.name);
+    }
+
+    /**
+     * Create a GifProvider from the current configuration.
+     */
+    public build() {
+        return new GifProvider(this.strategies);
+    }
+}
