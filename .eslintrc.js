852015cf2f3f2610d4d0319c91d3e90e80a98820
James Young <hello@jamesyoung.ch>
2020-10-21 19:34:16 +0200

    Add eslint and prettier
    - these run automatically with husky and lint-staged
    - linting can be run with `yarn lint`
    - pretty-printing can be run with `yarn pretty`
    - both formatting jobs can be run with `yarn format`

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint"
+    ]
+}
